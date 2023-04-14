0f4d7a8f46aeae1403e84b6b1ec8b463f1c8e1c4
Ramiro Monllor <ramiromonllor@gmail.com>
2023-04-13 21:32:06 -0300

    whatsapp

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -18,7 +18,8 @@ popupWhatsApp = () => {
   let msg = document.getElementById('whats-in').value;
   let relmsg = msg.replace(/ /g,"%20");
      
-   window.open('https://wa.me/56951289131?text='+relmsg, '_blank'); 
+  //  window.open('https://wa.me/56951289131?text='+relmsg, '_blank'); 
+   window.open('https://wa.me/56971748775?text='+relmsg, '_blank'); 
   
   });
 
